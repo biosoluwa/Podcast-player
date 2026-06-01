e245c8029bfa8557a72b233c1c0c7cba23019e99
biosoluwa <favbiose@gmail.com>
2026-06-01 15:13:22 +0100

    latest

--- public/index.js
+++ public/index.js
@@ -4,8 +4,38 @@ form.addEventListener('submit', async(e)=>{
     e.preventDefault()
     const searchInput = document.getElementById('search-input')
     if(searchInput.value){
-        const res = await fetch(`/api/search?q=${searchInput.value}`)
-        const data = await res.json()
-        console.log(data)
+        try{
+            const res = await fetch(`/api/search?q=${searchInput.value}`)
+            if(!res.ok){
+                throw new Error('Error getting podcast')
+            }
+            const data = await res.json()
+            console.log(data)
+            renderPodcasts(data)
+        }catch(err){
+            console.error(err)
+        }
     }
 })
+
+function renderPodcasts(podObj){
+    const podcasts = podObj.feeds
+    const container = document.getElementById('container')
+    container.innerHTML = podcasts.map((podcast)=>{
+        
+      return  `
+            <div class="card">
+                <div class="card-image">
+                    <img data-src="${podcast.image}" src="${podcast.image}" alt="podcast image" />
+                </div>
+                <div>
+                    <h2>${podcast.title}</h2>
+                    <p>${podcast.description}</p>
+                    <p>Episodes: <span> ${podcast.episodeCount} </span></p>
+                    <p> Newest Episode: <time datetime="${podcast.newestItemPubdate}"</time></p>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+            </div>
+        `
+    }).join('')
+}
--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import podcastIndexApi from 'podcast-index-api'
 
 dotenv.config()
-
-const podcastIndexApi = require('podcast-index-api')
-
-const api = podcastIndexApi(
-    process.env.PODCAST_INDEX_KEY,
-    process.env.PODCAST_INDEX_SECRET,
-    'my-app/1.0 (https://example.com)'
-)
-
-
 
 const app = express()
 const PORT = 8000
 
 app.use(express.static('public'))
+
+const api = podcastIndexApi(
+    process.env.PODCAST_INDEX_KEY,
+    process.env.PODCAST_INDEX_SECRET,
+    'podcast-player/1.0'
+)
+
+app.get('/api/search', async(req,res)=>{
+    try{
+        const results = await api.searchByTerm(req.query.q)
+        res.json(results)
+    }catch(err){
+        res.status(500).json({error: "Internal server error"})
+    }
+
+})
 
 app.listen(PORT, ()=> console.log('Server listening on PORT: ', PORT))
 
